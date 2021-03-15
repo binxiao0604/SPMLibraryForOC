99e8d99f6a4b3e54d63b529929ac088478123de8
zhangpeng <zhangpeng4@haier.com>
2021-03-15 19:10:43 +0800

    Argument 'dependencies' must precede argument 'publicHeadersPath'问题修复
    Change-Id: Ib7053fbcf5a9188c731906906bae02a86f7fca9c

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SPMLibraryForOC",
-            publicHeadersPath: "../SPMLibraryForOC",
-            dependencies: []),
+            dependencies: [],
+            publicHeadersPath: "../SPMLibraryForOC"),
         .testTarget(
             name: "SPMLibraryForOCTests",
             dependencies: ["SPMLibraryForOC"]),
