53ac3c80e21393d8e91bde797cb8520f4e298747
zhangpeng <zhangpeng4@haier.com>
2021-03-16 14:17:21 +0800

    path修改
    Change-Id: I37f23dfc59f0f9f1ebbc7bc88f75f83c79cd616e

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     targets: [
         .target(
             name: "SPMLibraryForOC",
-            publicHeadersPath: "../SPMLibraryForOC")
+            path: "Sources/SPMLibraryForOC"
+            publicHeadersPath: "../Sources/SPMLibraryForOC/include")
     ]
 )
